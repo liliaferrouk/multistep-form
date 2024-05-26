fad08f211893edeea3bcf4079562dd43f289af5e
liliaferrouk <ferrouklilia@gmail.com>
2024-05-26 23:09:49 +0200

    thankyou page done

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './ThankYou.css'
+import img_done from '../images/icon-thank-you.svg'
 
 function ThankYou() {
   return (
-    <div>ThankYou</div>
+    <div className='thankYouDiv'>
+        <img src={img_done} alt="" />
+        <h1>Thank you!</h1>
+        <p>Thanks for confirming your subscription! We hope you have fun 
+            using our platform. If you ever need support, please feel free 
+            to email us at support@loremgaming.com.</p>
+    </div>
   )
 }
 
