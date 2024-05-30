a796ac3df789f802191aaa19dff4d1e81d70a908
liliaferrouk <ferrouklilia@gmail.com>
2024-05-30 14:27:51 +0200

    css selected done

--- src/components/Pick.js
+++ src/components/Pick.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import img_check from '../images/icon-checkmark.svg'
 
 function Pick({id,title,details,priceMonthly,priceYearly,isMonthly}) {
     const [isSelected,setIsSelected] = useState(false)
   return (
-    <div className='pick-div'>
+    <div className='pick-div selected'>
         <div className='checkBox'>
-
+            <img src={img_check} alt="checkmark" />
         </div>
         <div className='info'>
             <h2>{title}</h2>
